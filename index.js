1240e0bd7cb6fee2969b722046133e409ee09fe8
Indunil1992 <indunil@adroitlogic.com>
2019-09-17 09:53:22 +0530

    add g storage

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.testgcp17function = require("./testgcp17/function.js").handler;
